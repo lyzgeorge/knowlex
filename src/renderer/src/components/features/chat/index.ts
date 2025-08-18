9479ba20a886cd2f5de51e104e3e7688d2bac4ea
George@Mac-Mini <lyz@GeorgedeMac-mini.local>
2025-08-18 18:38:36 +0800

    fix filecard

--- src/renderer/src/components/features/chat/index.ts
+++ src/renderer/src/components/features/chat/index.ts
@@ -1,9 +1,9 @@
 export { default as ChatInterface } from './ChatInterface'
 export { default as MessageList } from './MessageList'
 export { default as ChatInputBox } from './ChatInputBox'
-export { default as FilePreview } from './FilePreview'
+export { default as TempFileCard } from './TempFileCard'
 
 export type { ChatInterfaceProps } from './ChatInterface'
 export type { MessageListProps } from './MessageList'
 export type { ChatInputBoxProps } from './ChatInputBox'
-export type { FilePreviewProps } from './FilePreview'
+export type { TempFileCardProps } from './TempFileCard'
